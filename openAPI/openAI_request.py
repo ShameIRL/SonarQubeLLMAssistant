11c046b167d97eaabed1dbd594b93ceb2a26fe22
Matteo <109966451+ShameIRL@users.noreply.github.com>
2024-06-05 17:39:25 +0200

    Corrected Temperature openAI_request.py

--- openAPI/openAI_request.py
+++ openAPI/openAI_request.py
@@ -21,6 +21,6 @@ class getCompletion:
             messages=
                userMessage
             ,
-            temperature=0,
+            temperature=0.7,
         )
         return completion.choices[0].message
