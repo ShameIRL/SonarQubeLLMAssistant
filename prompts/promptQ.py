47b342d7e553a0de250f4a86f49c5439b229c672
ShameIRL <109966451+ShameIRL@users.noreply.github.com>
2024-06-26 16:06:40 +0200

    Added "promptQ.py", "variableQuestions.py"

---
+++ prompts/promptQ.py
@@ -0,0 +1,26 @@
+from variableQuestions import ApplicationServices, InternetExposure, PrivilegeRequirements, EnvironmentManagement, ProtectionConfig, EncryptionPractices, TrustedOrigins, FilePaths
+
+PROMPT = """
+You are going to assist with the analysis of a {HSorV}, but before performing any analysis, you are given some informations about the project you are going to analyze.
+Here are the Application Services present in the Project and the way they communicate:
+{ApplicationServices}
+Here is the way the Application is exposed to the Internet:
+{InternetExposure}
+Here are the Privilege Requirements in the Application:
+{PrivilegeRequirements}
+Here is how the Environment Variables are managed in the Application:
+{EnvironmentManagement}
+Here is how the Security Configurations are implemented in the Application:
+{ProtectionConfig}
+Here are the Encryption Practices adopted for Data Transits happening in the Application:
+{EncryptionPractices}
+Here are the project Trusted Origins and the way they are managed:
+{TrustedOrigins}
+Here is an extensive list of File Paths for each file in the Application:
+{FilePaths}
+
+Keep these informations in mind while assisting with the analysis of the {HSorV}.
+"""
+
+PROMPT_QV = PROMPT.format(HSorV="Security Hotspot", ApplicationServices=ApplicationServices, InternetExposure=InternetExposure, PrivilegedRequirements=PrivilegeRequirements, EnvironmentManagement=EnvironmentManagement, ProtectionConfig=ProtectionConfig, EncryptionPractices=EncryptionPractices, TrustedOrigins=TrustedOrigins, FilePaths=FilePaths)
+PROMPT_QHS = PROMPT.format(HSorV="Vulnerability", ApplicationServices=ApplicationServices, InternetExposure=InternetExposure, PrivilegedRequirements=PrivilegeRequirements, EnvironmentManagement=EnvironmentManagement, ProtectionConfig=ProtectionConfig, EncryptionPractices=EncryptionPractices, TrustedOrigins=TrustedOrigins, FilePaths=FilePaths)
---
+++ variableQuestions.py
@@ -0,0 +1,22 @@
+# List and Describe the different Application Services found in the project, separated by a comma,
+# then Explain the way they communicate, separating this part from the previous with a semicolon:
+ApplicationServices = ""
+# Describe the way the Application is exposed to the Internet, if it's not exposed, specify it:
+InternetExposure = ""
+# Describe the Privilege Requirements in the context of the project Application:
+PrivilegeRequirements = ""
+# Describe how the Environment Variables are managed in the context of the project Application:
+EnvironmentManagement = ""
+# List and Describe the different Security Configurations set ups that are implemented in the context
+# of the project Application, provide a description too:
+ProtectionConfig = ""
+# Describe the Encryption Practices adopted for the Data Traffic in the context of the project Application:
+EncryptionPractices = ""
+# Describe the Practices adopted to manage Secure Origins in the context of the project Application, 
+# along with Trusted Origins:
+TrustedOrigins = ""
+# Describe the way the files are organized in the application, prividing the PATH/TO/FILE.xx for each
+# file present in the application:
+FilePaths = """
+
+"""
